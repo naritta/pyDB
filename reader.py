0853f80b66e171bc79385657e9d39d47706e9922
naritta <narittan@gmail.com>
2022-12-16 01:45:22 +0900

    Add read for shared lock and exclusive lock

--- reader.py
+++ reader.py
@@ -2,13 +2,14 @@ from concurrent.futures import ThreadPoolExecutor
 import time
 import threading
 import json
+import fcntl, os
 
 class Reader():
     def __init__(self, lock):
         self.lock = lock
         self.data = None
 
-    def run(self, path):
+    def run_thread_lock(self, path):
         self.lock.acquire()
         data_json = ""
         # test for checking
@@ -20,12 +21,34 @@ class Reader():
         self.lock.release()
         return json.loads(data_json)
 
+    def run_shared_lock(self, path):
+        with open(path, 'r+') as f:
+            try:
+                fcntl.flock(f, fcntl.LOCK_SH)
+                # test for checking
+                time.sleep(1)
+                data_json = f.read()
+                print(json.loads(data_json))
+            except IOError:
+                print('shared_lock')
+
+    def run_exclusive_lock(self, path):
+        with open(path, 'r+') as f:
+            try:
+                fcntl.flock(f, fcntl.LOCK_EX)
+                # test for checking
+                time.sleep(1)
+                data_json = f.read()
+                print(json.loads(data_json))
+            except IOError:
+                print('exclusive_lock')
+
 if __name__ == "__main__":
     lock = threading.Lock()
     thread_pool = []
-    thread_pool.append(Reader(lock).run("testpath/test.json"))
-    thread_pool.append(Reader(lock).run("testpath/test.json"))
-    thread_pool.append(Reader(lock).run("testpath/test.json"))
+    thread_pool.append(Reader(lock).run_thread_lock("testpath/test.json"))
+    thread_pool.append(Reader(lock).run_thread_lock("testpath/test.json"))
+    thread_pool.append(Reader(lock).run_thread_lock("testpath/test.json"))
 
     with ThreadPoolExecutor(max_workers=3) as executor:
         for th in thread_pool:
